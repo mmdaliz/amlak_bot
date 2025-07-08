f7fa70f9f20f3892dfeb9cb966c78c0c10fd03da
mmdaliz <alizadehmohammad82@gmail.com>
2025-07-08 16:11:27 +0330

    bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,13 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
+TOKEN = "8174394810:AAGAk-D9HhbO3N3F8dV0NE2MrwUfT-pm_lY"
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("hiسلام! به ربات املاک اصفهان خوش اومدی 🌟")
+    await update.message.reply_text("سلام! ربات املاک اصفهان فعال است.")
 
 if __name__ == '__main__':
-    print("ربات با موفقیت اجرا شد ✅")
-    TOKEN = "8174394810:AAGAk-D9HhbO3N3F8dV0NE2MrwUfT-pm_lY"
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
+    print("✅ ربات با موفقیت اجرا شد...")
     app.run_polling()
